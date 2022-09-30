885d4ff022140314e03ae5a53853e4dc662721df
v.bianchi <valerio.bianchi@euforlegal.it>
2022-09-30 17:29:03 +0200

    first commit

---
+++ employee-manager-fe/employee-manager/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeService } from './employee.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [EmployeeService], // not really necessary, because it's providedIn: 'root'
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
